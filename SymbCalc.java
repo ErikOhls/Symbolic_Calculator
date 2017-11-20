6369cc581e149ff53ad49d6226b58ee384e1dae8
Erik <Ohls2nd@gmail.com>
2017-11-20 15:37:32 +0100

    byter dator

--- SymbCalc.java
+++ SymbCalc.java
@@ -1,18 +1,27 @@
+package symbolic;
+
 public class SymbCalc {
 
     public static void main() {
 
+        // Hur funkar Parser?
+        // Skriv den själv!
         Parser p = new Parser();
+
+        // Hur funkar HashMap?
+        // En lista med alla variabler
+        // <variable name, value>
         Map<String,Sexpr> variables = new HashMap<String,Sexpr>();
 
         while (true)
             try {
                 System.out.print("? ");
+                //Scanner i p???
                 Sexpr e = p.statement();
-                System.out.println("Inläst uttryck: " + e)  // För kontroll
+                System.out.println("Inläst uttryck: " + e);  // För kontroll
                     System.out.println(e.eval(variables));
-            }/* catch (...) {
-                ...
-                } */
+                // Måste ha en catch?
+            } catch (Exception e) {
+            }
     }
 }
